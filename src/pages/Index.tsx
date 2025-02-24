01d86fe3d05d963bb0b7e3300abb0d5deeb7c1cf
Ahmd Abdo <rehlathadaf@gmail.com>
2025-02-24 17:25:26 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -30,7 +30,7 @@ const Index = () => {
             className="bg-[#ff535f] hover:bg-[#e04a56] text-white"
           >
             <Plus className="w-5 h-5 mr-2" />
-            إضافة امتنان جديد
+             امتنان جديد
           </Button>
         </div>
 
