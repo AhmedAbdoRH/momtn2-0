7d7e890b8c6f5e67c3c0a9d31990777d32ec4ffd
Ahmd Abdo <rehlathadaf@gmail.com>
2025-04-10 17:07:26 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -98,7 +98,7 @@ const Index = () => {
             >
               <Plus className={`w-5 h-5 mr-2 transition-transform duration-300 ${btnAnimation ? 'rotate-180' : ''}`} />
               إضافة امتنان جديد
-            </Button>
+            </Button-red>
           </div>
 
           <PhotoGrid />
